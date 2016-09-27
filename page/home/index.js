abf14e515012c8855fb7b4340a1868a2218ff260
kongwenhua <kongwenhua@yaochufa.com>
2016-09-27 15:00:45 +0800

    修改

--- page/home/index.js
+++ page/home/index.js
@@ -1,3 +1,14 @@
 Page({
-	data: {}
+	data: {
+		list :[1,2,3,4,5,6,7],
+		imgUrls: [
+	      'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+	      'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+	      'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
+	    ],
+	    indicatorDots: false,
+	    autoplay: false,
+	    interval: 5000,
+	    duration: 1000
+	}
 });
